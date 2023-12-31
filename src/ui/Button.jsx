5e010978561da4c08e1510f337f52b8f9693c21b
Tedo <tedojanelidze7@gmail.com>
2023-12-31 19:48:27 +0400

    added customm style in config

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled }) {
+export default function Button({ children, disabled, to }) {
   const className =
     "inline-block rounded-full bg-yellow-400 px-4 py-4 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
 
