0ff26efd403391d1f33f257c11465f98545d55ec
Tedo <tedojanelidze7@gmail.com>
2023-12-31 12:10:26 +0400

    added tailwindcss

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+
+export default function Header() {
+  return (
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
+
+      <p>Tedo</p>
+    </header>
+  );
+}
